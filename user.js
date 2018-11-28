42f428e9cf3c10c01a37988e911f83b22c7eaba2
YegorMishin <45308005+YegorMishin@users.noreply.github.com>
2018-11-29 01:04:37 +0400

    Update user.js

--- user.js
+++ user.js
@@ -8,27 +8,4 @@ $DOC.sections['fixed-top-bar'] =
  * [Основная часть]({{=$DOC.root}}установка-и-обновление)\n\
  * [список информационных ресурсов]({{=$DOC.root}}редактор)\n\
  * [об авторе]({{=$DOC.root}}формат-файлов)\n\
- * [FAQ]({{=$DOC.root}}faq)\n\
 [/navbar]';
-
-$DOC.sections['header-panel'] =
-'# Markdown webdocs\n\
-Система подготовки веб-справки и документации  \n\
-Редактор документов с Markdown разметкой';
-
-
-if ($OPT.editable) {
-    $DOC.sections['footer-panel'] =
-'[footer-layout scheme=line]\
-* © 2013 [docs-ru on GitHub](https://github.com/aplib/docs-ru) Artistic license 2.0\n\
-***\n\
-* [Редактировать эту страницу](?edit)\
-[/footer-layout]';
-} else {
-    $DOC.sections['footer-panel'] =
-'[footer-layout scheme=line]\
-* © 2013 [docs-ru on GitHub](https://github.com/aplib/docs-ru) Artistic license 2.0\
-[/footer-layout]';
-}
-
-})();
